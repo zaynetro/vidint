84eef7bf8a54b5953d7f099ce6bd3bf30bb75e11
Roman Zaynetdinov <romanznet@gmail.com>
2015-02-20 15:30:55 +0200

    Initial commit

---
+++ public/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * Front page
+ */
+
+;(function (w, d, undefined) {
+
+  window.onload = function () {
+    var initBtn = d.querySelector('#initBtn');
+
+    initBtn.addEventListener('click', initVideo, false);
+
+    function initVideo() {
+      
+    }
+  };
+
+})(window, document, undefined);
