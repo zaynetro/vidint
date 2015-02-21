b88b43c429fc180be9a0671c7a35ddd5efa3f39e
Roman Zaynetdinov <romanznet@gmail.com>
2015-02-21 03:04:38 +0200

    Update main js

--- public/js/main.js
+++ public/js/main.js
@@ -2,108 +2,112 @@
  * Main page
  */
 
-Parse.initialize("CM0kieoQyGZwRayG9jUT7RLYwaUujPVa9B9GrsyH", "y6wWLtz54RA2ALBxYH61fOo2wsph9HcwvqH6tdmk");
+;(function () {
 
-var startBtn = document.querySelector('#startBtn');
-var stopBtn = document.querySelector('#stopBtn');
-var video = document.querySelector('#video');
-var uploadBtn = document.querySelector('#uploadBtn');
-var replyLink = document.querySelector('#replyLink');
-var answersLink = document.querySelector('#answersLink');
-var companyName = document.querySelector('#companyName');
+  Parse.initialize("CM0kieoQyGZwRayG9jUT7RLYwaUujPVa9B9GrsyH", "y6wWLtz54RA2ALBxYH61fOo2wsph9HcwvqH6tdmk");
 
-startBtn.onclick = function () {
-  startBtn.disabled = true;
-  stopBtn.disabled = false;
-  uploadBtn.disabled = true;
+  var startBtn = document.querySelector('#startBtn');
+  var stopBtn = document.querySelector('#stopBtn');
+  var video = document.querySelector('#video');
+  var uploadBtn = document.querySelector('#uploadBtn');
+  var replyLink = document.querySelector('#replyLink');
+  var answersLink = document.querySelector('#answersLink');
+  var companyName = document.querySelector('#companyName');
 
-  captureUserMedia(function (stream) {
-    window.audioVideoRecorder = window.RecordRTC(stream, {
-      type : 'video' // don't forget this; otherwise you'll get video/webm instead of audio/ogg
-    });
-    window.audioVideoRecorder.startRecording();
-  });
-};
+  startBtn.onclick = function () {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    uploadBtn.disabled = true;
 
-stopBtn.onclick = function () {
-   stopBtn.disabled = true;
-   startBtn.disabled = false;
-   uploadBtn.disabled = false;
-
-  window.audioVideoRecorder.stopRecording(function (url) {
-    //  downloadURL.innerHTML = '<a href="' + url + '" download="RecordRTC.webm" target="_blank">Save RecordRTC.webm to Disk!</a>';
-    video.src = url;
-    video.muted = false;
-    video.play();
-
-    video.onended = function() {
-      video.pause();
-
-      // dirty workaround for: "firefox seems unable to playback"
-      // video.src = URL.createObjectURL(audioVideoRecorder.getBlob());
-    };
-
-    // var recordedBlob = window.audioVideoRecorder.getBlob();
-    // window.audioVideoRecorder.getDataURL(function (dataURL) {
-    //   console.log(dataURL);
-    // });
-    // console.log(recordedBlob);
-  });
-};
-
-function captureUserMedia(callback) {
-   navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
-   navigator.getUserMedia({
-     audio : true,
-     video : true
-   }, function(stream) {
-     video.src = URL.createObjectURL(stream);
-     video.muted = true;
-     video.controls = true;
-    //  videoElement.play();
-
-     callback(stream);
-   }, function (error) {
-     console.error(error);
-   });
-}
-
-uploadBtn.onclick = function () {
-  // var blob = window.audioVideoRecorder.getBlob();
-  //
-  // var reader = new window.FileReader();
-  // reader.readAsDataURL(blob);
-  // reader.onloadend = function() {
-  //   base64data = reader.result;
-  //   console.log(base64data);
-  //   video.src = base64data;
-  // };
-
-  window.audioVideoRecorder.getDataURL(function (dataURL) {
-
-    var file = new Parse.File('video.webm', { base64: dataURL });
-    file.save().then(function (saved) {
-
-      var interview = new Parse.Object('Interview');
-      interview.set('video', file);
-      interview.set('company', companyName.value);
-      interview.save(null, {
-        success : function (interview) {
-          replyLink.value = pageUrl() + 'reply.html#to=' + interview.id;
-          answersLink.value = pageUrl() + 'answers.html#to=' + interview.id;
-        },
-
-        error : function (interview, error) {
-          console.log(error);
-        }
+    captureUserMedia(function (stream) {
+      window.audioVideoRecorder = window.RecordRTC(stream, {
+        type : 'video' // don't forget this; otherwise you'll get video/webm instead of audio/ogg
       });
-
-    }, function (error) {
-      console.log(error);
+      window.audioVideoRecorder.startRecording();
     });
-  });
+  };
 
-  function pageURL() {
-    return location.protocol + '//' + location.host + '/';
+  stopBtn.onclick = function () {
+     stopBtn.disabled = true;
+     startBtn.disabled = false;
+     uploadBtn.disabled = false;
+
+    window.audioVideoRecorder.stopRecording(function (url) {
+      //  downloadURL.innerHTML = '<a href="' + url + '" download="RecordRTC.webm" target="_blank">Save RecordRTC.webm to Disk!</a>';
+      video.src = url;
+      video.muted = false;
+      video.play();
+
+      video.onended = function() {
+        video.pause();
+
+        // dirty workaround for: "firefox seems unable to playback"
+        // video.src = URL.createObjectURL(audioVideoRecorder.getBlob());
+      };
+
+      // var recordedBlob = window.audioVideoRecorder.getBlob();
+      // window.audioVideoRecorder.getDataURL(function (dataURL) {
+      //   console.log(dataURL);
+      // });
+      // console.log(recordedBlob);
+    });
+  };
+
+  function captureUserMedia(callback) {
+     navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+     navigator.getUserMedia({
+       audio : true,
+       video : true
+     }, function(stream) {
+       video.src = URL.createObjectURL(stream);
+       video.muted = true;
+       video.controls = true;
+      //  videoElement.play();
+
+       callback(stream);
+     }, function (error) {
+       console.error(error);
+     });
   }
-};
+
+  uploadBtn.onclick = function () {
+    // var blob = window.audioVideoRecorder.getBlob();
+    //
+    // var reader = new window.FileReader();
+    // reader.readAsDataURL(blob);
+    // reader.onloadend = function() {
+    //   base64data = reader.result;
+    //   console.log(base64data);
+    //   video.src = base64data;
+    // };
+
+    window.audioVideoRecorder.getDataURL(function (dataURL) {
+
+      var file = new Parse.File('video.webm', { base64: dataURL });
+      file.save().then(function (saved) {
+
+        var interview = new Parse.Object('Interview');
+        interview.set('video', file);
+        interview.set('company', companyName.value);
+        interview.save(null, {
+          success : function (interview) {
+            replyLink.value = pageUrl() + 'reply.html#to=' + interview.id;
+            answersLink.value = pageUrl() + 'answers.html#to=' + interview.id;
+          },
+
+          error : function (interview, error) {
+            console.log(error);
+          }
+        });
+
+      }, function (error) {
+        console.log(error);
+      });
+    });
+
+    function pageUrl() {
+      return location.protocol + '//' + location.host + '/';
+    }
+  };
+
+})();
