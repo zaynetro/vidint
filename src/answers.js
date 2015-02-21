f0475b7793ab303ad0d7f4d146cd44ecf2cf48cf
Roman Zaynetdinov <romanznet@gmail.com>
2015-02-21 10:16:29 +0200

    Use npm parse module

---
+++ src/answers.js
@@ -0,0 +1,72 @@
+/**
+ * Answers page
+ */
+
+var Parse = require('parse').Parse;
+
+Parse.initialize("CM0kieoQyGZwRayG9jUT7RLYwaUujPVa9B9GrsyH", "y6wWLtz54RA2ALBxYH61fOo2wsph9HcwvqH6tdmk");
+
+var companyName = document.querySelector('#companyName');
+var question = document.querySelector('#question');
+var answersNode = document.querySelector('#answers');
+
+var hash = location.hash;
+
+if(!hash.length) return console.log('Wrong address');
+
+// Remove hash mark
+hash = hash.slice(1);
+
+var params = hash.split('&');
+var interviewId;
+
+params.forEach(function (param) {
+  var keys = param.split('=');
+  if(keys[0] == 'to') {
+    interviewId = keys[1];
+  }
+});
+
+if(!interviewId) return console.log('Wrong address');
+
+var Interview = Parse.Object.extend('Interview');
+var query = new Parse.Query(Interview);
+query.get(interviewId, {
+  success : function (interview) {
+    companyName.textContent = interview.get('company');
+    question.src = interview.get('video')._url;
+  },
+
+  error : function (_, error) {
+    console.log(error);
+  }
+});
+
+var Answer = Parse.Object.extend('Answer');
+var ansQuery = new Parse.Query(Answer);
+ansQuery.equalTo('interview', interviewId);
+ansQuery.descending('createdAt');
+ansQuery.find({
+  success : function (answers) {
+    console.log('Fetched', answers.length);
+    answers.forEach(function (answer) {
+      var li = document.createElement('li');
+
+      var name = document.createElement('div');
+      name.textContent = answer.get('name');
+
+      var video = document.createElement('video');
+      video.src = answer.get('video')._url;
+      video.controls = true;
+
+      li.appendChild(name);
+      li.appendChild(video);
+
+      answersNode.appendChild(li);
+    });
+  },
+
+  error : function (error) {
+    console.log(error);
+  }
+});
---
+++ src/main.js
@@ -0,0 +1,113 @@
+/**
+ * Main page
+ */
+
+var Parse = require('parse').Parse;
+
+Parse.initialize("CM0kieoQyGZwRayG9jUT7RLYwaUujPVa9B9GrsyH", "y6wWLtz54RA2ALBxYH61fOo2wsph9HcwvqH6tdmk");
+
+var startBtn = document.querySelector('#startBtn');
+var stopBtn = document.querySelector('#stopBtn');
+var video = document.querySelector('#video');
+var uploadBtn = document.querySelector('#uploadBtn');
+var replyLink = document.querySelector('#replyLink');
+var answersLink = document.querySelector('#answersLink');
+var companyName = document.querySelector('#companyName');
+
+var recorder;
+
+startBtn.onclick = function () {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  uploadBtn.disabled = true;
+
+  captureUserMedia(function (stream) {
+    recorder = RecordRTC(stream, {
+      type : 'video' // don't forget this; otherwise you'll get video/webm instead of audio/ogg
+    });
+    recorder.startRecording();
+  });
+};
+
+stopBtn.onclick = function () {
+   stopBtn.disabled = true;
+   startBtn.disabled = false;
+   uploadBtn.disabled = false;
+
+  recorder.stopRecording(function (url) {
+    //  downloadURL.innerHTML = '<a href="' + url + '" download="RecordRTC.webm" target="_blank">Save RecordRTC.webm to Disk!</a>';
+    video.src = url;
+    video.muted = false;
+    video.play();
+
+    video.onended = function() {
+      video.pause();
+
+      // dirty workaround for: "firefox seems unable to playback"
+      // video.src = URL.createObjectURL(audioVideoRecorder.getBlob());
+    };
+
+    // var recordedBlob = window.audioVideoRecorder.getBlob();
+    // window.audioVideoRecorder.getDataURL(function (dataURL) {
+    //   console.log(dataURL);
+    // });
+    // console.log(recordedBlob);
+  });
+};
+
+function captureUserMedia(callback) {
+   navigator.getUserMedia = navigator.mozGetUserMedia || navigator.webkitGetUserMedia;
+   navigator.getUserMedia({
+     audio : true,
+     video : true
+   }, function(stream) {
+     video.src = URL.createObjectURL(stream);
+     video.muted = true;
+     video.controls = true;
+    //  videoElement.play();
+
+     callback(stream);
+   }, function (error) {
+     console.error(error);
+   });
+}
+
+uploadBtn.onclick = function () {
+  // var blob = window.audioVideoRecorder.getBlob();
+  //
+  // var reader = new window.FileReader();
+  // reader.readAsDataURL(blob);
+  // reader.onloadend = function() {
+  //   base64data = reader.result;
+  //   console.log(base64data);
+  //   video.src = base64data;
+  // };
+
+  recorder.getDataURL(function (dataURL) {
+
+    var file = new Parse.File('video.webm', { base64: dataURL });
+    file.save().then(function (saved) {
+
+      var interview = new Parse.Object('Interview');
+      interview.set('video', file);
+      interview.set('company', companyName.value);
+      interview.save(null, {
+        success : function (interview) {
+          replyLink.value = pageUrl() + 'reply.html#to=' + interview.id;
+          answersLink.value = pageUrl() + 'answers.html#to=' + interview.id;
+        },
+
+        error : function (interview, error) {
+          console.log(error);
+        }
+      });
+
+    }, function (error) {
+      console.log(error);
+    });
+  });
+
+  function pageUrl() {
+    return location.protocol + '//' + location.host + '/';
+  }
+};
